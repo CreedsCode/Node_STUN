1046d72155fc0df01d6d7f9fb3e723d59433adfd
facewreck <Thoughtscript@thoughtscript.io>
2016-11-27 02:14:30 -0600

    Heroku process.env.port

--- config.js
+++ config.js
@@ -1,25 +1,25 @@
 module.exports = {
-	server: {
-		port: 8888
-	},
-	stun: {
-		primary: {
-			host: '127.0.0.1',
-			port: 3478
-		},
-		secondary: {
-			host: '127.0.0.2',
-			port: 3479
-		},
-		defaults: {
-			primary: {
-				host: '127.0.0.1',
-				port: 3478
-			},
-			secondary: {
-				host: '127.0.0.2',
-				port: 3479
-			}
-		}
-	}
+    server: {
+        port: process.env.PORT || 8888
+    },
+    stun: {
+        primary: {
+            host: '127.0.0.1',
+            port: 3478
+        },
+        secondary: {
+            host: '127.0.0.2',
+            port: 3479
+        },
+        defaults: {
+            primary: {
+                host: '127.0.0.1',
+                port: 3478
+            },
+            secondary: {
+                host: '127.0.0.2',
+                port: 3479
+            }
+        }
+    }
 };
